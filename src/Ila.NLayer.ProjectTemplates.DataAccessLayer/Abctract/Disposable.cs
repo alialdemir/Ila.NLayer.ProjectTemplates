aab406cf10b877d8b86cb1be907ca4a1dd59d98f
Ali Aldemir <ali.aldemir@portalgrup.com>
2021-01-28 19:46:50 +0300

    Disposable

---
+++ src/Ila.NLayer.ProjectTemplates.DataAccessLayer/Abctract/Disposable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ila.NLayer.ProjectTemplates.DataAccessLayer.Abctract
+{
+    public class Disposable : IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose used for the objects you want to.
+        /// </summary>
+        /// <param name="disposing">Disposing</param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+    }
+}
