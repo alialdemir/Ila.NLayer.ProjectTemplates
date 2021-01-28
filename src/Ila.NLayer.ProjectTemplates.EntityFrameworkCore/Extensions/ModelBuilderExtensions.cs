58cf29fd98893cbafd2523e2c82521eba38bc764
Ali Aldemir <ali.aldemir@portalgrup.com>
2021-01-28 21:15:07 +0300

    soft deletable ModelBuilderExtensions

---
+++ src/Ila.NLayer.ProjectTemplates.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Ila.NLayer.ProjectTemplates.DataAccessLayer.Entities.Base.EntityWithDeletableBase;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Ila.NLayer.ProjectTemplates.EntityFrameworkCore.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddGlobalSoftDeletableFilter(this ModelBuilder modelBuilder)
+        {
+            foreach (var type in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(IEntityWithDeletableBase).IsAssignableFrom(type.ClrType))
+                    modelBuilder.SetIEntityWithDeletableFilter(type.ClrType);
+            }
+        }
+
+        public static void SetIEntityWithDeletableFilter(this ModelBuilder modelBuilder, Type entityType)
+        {
+            SetIEntityWithDeletableMethod
+                .MakeGenericMethod(entityType)
+                .Invoke(null, new object[] { modelBuilder });
+        }
+
+        private static readonly MethodInfo SetIEntityWithDeletableMethod = typeof(ModelBuilderExtensions)
+                   .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                   .Single(t => t.IsGenericMethod && t.Name == "SetIEntityWithDeletableFilter");
+
+        public static void SetIEntityWithDeletableFilter<TEntity>(this ModelBuilder modelBuilder)
+            where TEntity : class, IEntityWithDeletableBase
+        {
+            modelBuilder.Entity<TEntity>().HasQueryFilter(x => !x.Deleted);
+        }
+    }
+}
